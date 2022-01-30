a2703d450a06c3fc5e5753bef8205f906cb33524
Daniel Hey <dhey@groupon.com>
2022-01-30 11:24:50 -0600

    update typescript and more

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,26 +3,55 @@ import { Earth } from "../src/Earth";
 import { Asteroid } from "../src/Asteroid";
 import ReactTooltip from "react-tooltip";
 
+export interface AsteroidType {
+  id: string
+  name: string
+  absolute_magnitude_h: number
+  is_potentially_hazardous_asteroid: boolean,
+  close_approach_data: CloseApproachData[],
+  miss_distance: number,
+  css_position: number
+}
+
+interface CloseApproachData {
+  miss_distance: {
+    miles: number
+  }
+}
+
 export const loader = async () => {
   const date = `${new Date().getFullYear()}-${("0" + (new Date().getMonth() + 1)).slice(-2)}-${("0" + new Date().getDate()).slice(-2)}`
   const response = await fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${date}&end_date=${date}&api_key=${process.env.API_KEY}`);
   const asteroids = await response.json();
-  return asteroids.near_earth_objects[date].map(a => {
-    a.top = Math.floor(Math.random() * 700)
-    return a;
+  return asteroids.near_earth_objects[date].map((a: AsteroidType) => {
+    return {
+      id: a.id,
+      name: a.name,
+      absolute_magnitude_h: a.absolute_magnitude_h,
+      is_potentially_hazardous_asteroid: a.is_potentially_hazardous_asteroid,
+      miss_distance: a.close_approach_data && a.close_approach_data.length ? a.close_approach_data[0].miss_distance.miles : 0,
+      css_position: Math.floor(Math.random() * 700)
+    };
   });
 };
 
 export default function Index() {
   const asteroids = useLoaderData();
-  console.log(asteroids[0])
   return (
-      <div>
+      <>
         <ReactTooltip multiline={true} />
+        <h1 style={{
+          textAlign: 'center',
+          position: 'fixed',
+          left: 0,
+          bottom: 0,
+          height: 30,
+          width: '100%'
+        }}>Asteroids near Earth, today.</h1>
         <Earth />
-          {asteroids.map(asteroid => (
-            <Asteroid asteroid={asteroid} />
+          {asteroids.map((asteroid: AsteroidType) => (
+            <Asteroid asteroid={asteroid} key={asteroid.id}/>
           ))}
-      </div>
+      </>
   );
 }
--- app/src/Asteroid.tsx
+++ app/src/Asteroid.tsx
@@ -1,15 +1,21 @@
-export function Asteroid({ asteroid }) {
+import type { AsteroidType } from '../routes/index';
+
+export function Asteroid({ asteroid }: { asteroid: AsteroidType }) {
   return <div
     key={asteroid.id}
-    data-tip={[`Name: ${asteroid.name}`, `Is Hazardous?: ${asteroid.is_potentially_hazardous_asteroid}`].join('<br />')}
+    data-tip={[
+      `Name: ${asteroid.name}`,
+      `Potentially Hazardous?: ${asteroid.is_potentially_hazardous_asteroid ? 'Yes' : 'No'}`,
+      `Miss Distance: ${Math.floor(asteroid.miss_distance).toLocaleString("en-US")} miles`
+    ].join('<br />')}
     style={{
       position: 'absolute',
       backgroundColor: 'grey',
       borderRadius: '50%',
       width: Math.abs(asteroid.absolute_magnitude_h),
       height: Math.abs(asteroid.absolute_magnitude_h),
-      left: Math.abs(asteroid.close_approach_data[0].miss_distance.miles / 10000),
-      top: asteroid.top
+      left: `calc(${Math.abs(asteroid.miss_distance / 100000)}px + 10vw)`,
+      top: `calc(${Math.abs(asteroid.css_position)}px + 10vh)`
     }}
   />
 }
