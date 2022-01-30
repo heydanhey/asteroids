32a11b40a6f94974026ee35a9aec1691a20974d5
Daniel Hey <dhey@groupon.com>
2022-01-30 13:34:55 -0600

    add simple error handling

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -24,16 +24,21 @@ export const loader = async () => {
   const date = `${new Date().getFullYear()}-${("0" + (new Date().getMonth() + 1)).slice(-2)}-${("0" + new Date().getDate()).slice(-2)}`
   const response = await fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${date}&end_date=${date}&api_key=${process.env.API_KEY}`);
   const asteroids = await response.json();
-  return asteroids.near_earth_objects[date].map((a: AsteroidType) => {
-    return {
-      id: a.id,
-      name: a.name,
-      absolute_magnitude_h: a.absolute_magnitude_h,
-      is_potentially_hazardous_asteroid: a.is_potentially_hazardous_asteroid,
-      miss_distance: a.close_approach_data && a.close_approach_data.length ? a.close_approach_data[0].miss_distance.miles : 0,
-      css_position: Math.floor(Math.random() * 700)
-    };
-  });
+  try {
+    return asteroids.near_earth_objects[date]
+      .map((a: AsteroidType) => {
+        return {
+          id: a.id,
+          name: a.name,
+          absolute_magnitude_h: a.absolute_magnitude_h,
+          is_potentially_hazardous_asteroid: a.is_potentially_hazardous_asteroid,
+          miss_distance: a.close_approach_data && a.close_approach_data.length ? a.close_approach_data[0].miss_distance.miles : 0,
+          css_position: Math.floor(Math.random() * 700)
+        };
+      });
+  } catch {
+    return [];
+  }
 };
 
 export default function Index() {
